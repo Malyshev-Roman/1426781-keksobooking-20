eec90401685977fa45de08d4a98d97b678245f16
Malyshev-Roman <roman.malisev@mail.ru>
2020-06-24 00:36:25 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/data.js
+++ js/data.js
@@ -22,6 +22,19 @@
         copyArr[j] = temp;
       }
       return copyArr;
+    },
+
+    errorHandler: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '26px';
+      node.style.fontFamily = '"Roboto", "Arial", sans-serif';
+      node.style.color = 'black';
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
     }
   };
 
